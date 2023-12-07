e6360b5fef1a5234523fdd26e406d6c2bf9fde6c
mudapa <mudanpa@gmail.com>
2023-12-07 17:49:30 +0700

    Add Hero widget to display article image

--- lib/ui/pages/news_list_page.dart
+++ lib/ui/pages/news_list_page.dart
@@ -43,10 +43,14 @@ class NewsListPage extends StatelessWidget {
       },
       contentPadding:
           const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      leading: Image.network(
-        article.urlToImage!,
-        width: 100,
-        errorBuilder: (ctx, error, _) => const Center(child: Icon(Icons.error)),
+      leading: Hero(
+        tag: article.urlToImage!,
+        child: Image.network(
+          article.urlToImage!,
+          width: 100,
+          errorBuilder: (ctx, error, _) =>
+              const Center(child: Icon(Icons.error)),
+        ),
       ),
       title: Text(article.title!),
       subtitle: Text(article.author!),
